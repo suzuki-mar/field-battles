303e462496d5039e2bdb7f2bd95eceaba5a807f1
suzuki-mar <supasu145@gmail.com>
2021-08-13 08:56:36 +0900

    annotateの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# == Route Map
+#
+# ruby/3.0.2 isn't supported by this pry-doc version
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
