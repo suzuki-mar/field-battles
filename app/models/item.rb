f3cf82d1644c89066cd8f99247a1f2d6b5383085
suzuki-mar <supasu145@gmail.com>
2021-08-14 16:14:00 +0900

    デフォルトのアイテムを追加するメソッドの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,14 +6,28 @@
 #
 #  id           :integer          not null, primary key
 #  effect_value :integer          not null
+#  kind         :integer          not null
 #  name         :string           not null
 #  point        :integer          not null
-#  type         :integer          not null
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #
 class Item < ApplicationRecord
   has_one :item_stock, dependent: :destroy
 
-  enum types: { first_aid_kit: 0, drink: 1, weapone: 2 }
+  enum kinds: { first_aid_kit: 0, drink: 1, weapone: 2 }
+  
+  module Name 
+    FIJI_WATER = "Fiji Water"
+    CAMPBELL_SOUP = "Campbell Soup"
+    FIRST_AID_POUCH = "First Aid Pouch"
+    AK47 = "AK47"
+  end
+  
+  def self.create_initial_items
+    create(name: Name::FIJI_WATER, effect_value: 3, point: 14, kind: Item.kinds[:drink])
+    create(name: Name::CAMPBELL_SOUP, effect_value: 2, point: 12, kind: Item.kinds[:drink])
+    create(name: Name::FIRST_AID_POUCH, effect_value: 1, point: 10, kind: Item.kinds[:first_aid_kit])
+    create(name: Name::AK47, effect_value: 1, point: 8, kind: Item.kinds[:weapone])
+  end
 end
