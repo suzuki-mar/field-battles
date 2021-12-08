cd97309d8ec5514ea7c005c69afc1c3dbb5795f6
suzuki masayuki <supasu145@gmail.com>
2021-12-08 11:23:23 +0900

    ApplicationServiceを削除してコントローラーにユースケースを作成するのを移す (PlayerController::update_inventory以外) (#6)
    * ApplicationServiceを削除した
    
    * filedを完了した
    
    * PlayerControllerの対応
    
    update_inventoryはプライベートメソッドがあるため一旦保留

--- spec/requests/filed_spec.rb
+++ spec/requests/filed_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe 'Filed', type: :request do
     subject { get '/filed', headers: headers }
 
     before do
-      SetUpper.prepare_filed
+      SetUpper.prepare_items
 
-      create(:player, :survivor)
-      create(:player, :zombie)
+      create_list(:player, 6, :survivor)
+      create_list(:player, 3, :zombie)
 
       item_params = [
         { name: Item::Name::FIJI_WATER, count: 2 },
@@ -21,10 +21,23 @@ RSpec.describe 'Filed', type: :request do
       Inventory.create_for_newcomers(Player.last.id, item_params)
     end
 
-    it 'レポートの計算ができていること' do
+    # 計算自体はサービスクラスに異常している
+    it 'プレイ人口の割合が計算できていること' do
       subject
-      
-      expect(JsonParserSupport.response_body(response)).to have_key(:infected_percentage)
+      json = JSON.parse(response.body, {symbolize_names: true})
+
+      expect(json[:noninfected_percentage]).not_to be_nil
+      expect(json[:infected_percentage]).not_to be_nil
+      expect(json[:infected_percentage]).not_to be_nil
+    end
+
+    it 'アイテムの計算ができていること' do
+      subject
+
+      json = JSON.parse(response.body, {symbolize_names: true})
+
+      expect(json[:average_count_per_items]).not_to be_nil
+      expect(json[:wasted_item_points]).not_to be_nil
     end
 
     it_behaves_like "returns http success"
@@ -33,29 +46,43 @@ RSpec.describe 'Filed', type: :request do
   describe 'PUT /current_location' do
     subject { put '/filed/current_location', headers: headers }
 
+    let(:survivor_player) { create(:player, :survivor) }
+
     before do
-      create(:player, :survivor, current_lat: 0, current_lon: 0)
+      zombie_player = create(:player, :zombie)
+      survivor_player.update(current_lat: zombie_player.current_lat, current_lon: zombie_player.current_lon)
+      
+      # 乱数での判定なためモックを使用している
+      # TODO: 意味を理解するまで時間がかかるのでヘルパーにしたい
+      allow_any_instance_of(Location).to receive(:can_sight?).and_return(true)
+    end
+
+    it '移動していること' do
+      before_location = survivor_player.current_location
+      subject
+      after_location = survivor_player.reload.current_location
+
+      expect(after_location.equal(before_location)).to eq(false)
+    end
+
+    it 'ゾンビに襲われていること' do
+      subject
+      expect(Player.find(survivor_player.id).status).to eq(Player.statuses[:death])
     end
 
     it_behaves_like "returns http success"
 
-    it 'ユースケースが実行されていること' do
-      subject
-
-      location = Player.first.current_location
-      expect(location.lat).not_to eq(0)
-      expect(location.lon).not_to eq(0)
-    end
   end
 
   describe 'PUT /infectio' do
     subject { put '/filed/infection', headers: headers }
 
     before do
-      create_list(:player, 30, :survivor)
+      # 一人は感染する感染する生存者を発生させたいので多めに作成している
+      create_list(:player, 20, :survivor)
     end
 
-    it 'ユースケースが実行されていること' do
+    it 'ゾンビになった生存者がいること' do
       subject
 
       expect(Player.exists?(status: Player.statuses[:zombie])).to eq(true)
