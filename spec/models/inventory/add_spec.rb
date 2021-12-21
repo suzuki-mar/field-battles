435e9af82672e0fa3bddddc20af157cff643a09f
suzuki-mar <supasu145@gmail.com>
2021-12-21 17:42:50 +0900

    addのテストを移した

--- spec/models/inventory/add_spec.rb
+++ spec/models/inventory/add_spec.rb
@@ -21,103 +21,41 @@ RSpec.describe Inventory, type: :model do
       inventory
     end
 
-    context('存在しないアイテムを追加する場合') do
-      it '在庫を新規登録していること' do
-        subject
-        item_stock = inventory.stocks.first
-        expect(item_stock.item.name).to eq(item_name)
-        expect(item_stock.stock_count).to eq(1)
-      end
-    end
-
-    context('同じアイテムがすでに存在する場合') do
-      before do
-        item = Item.where(name: item_name).first
-        create(:item_stock, player: player, item: item, stock_count: 1)
-      end
-
-      it '在庫のカウントが増えていること' do
-        subject
-        item_stock = inventory.stocks.first
-        expect(item_stock.stock_count).to eq(2)
-      end
+    it '在庫を登録していること' do
+      subject
+      item_stock = inventory.stocks.first
+      expect(item_stock.item.name).to eq(item_name)
+      expect(item_stock.stock_count).to eq(1)
     end
   end
 
-  describe 'ロールバック' do
-    subject do
-      inventory.add!('Unknown Item', 3)
-    end
-
-    it '例外が発生していること' do
-      expect do
-        subject
-      end.to raise_error(ActiveRecord::Rollback)
-    end
-  end
-
-  describe('パラメーターの異常系') do
-    subject do
-      ActiveRecord::Base.transaction do
-        inventory.add!(params[:item_name], params[:count])
+  describe('異常系') do
+    describe 'ロールバックの確認' do 
+      subject do
+        inventory.add!('Unknown Item', 3)
       end
 
-      error = inventory.errors.first
-      error.messages.first
-    end
-
-    let(:params) do
-      {
-        item_name: item_name,
-        count: 1
-      }
-    end
-
-    context('存在しないアイテム名のものを追加しようとした場合') do
-      before do
-        params[:item_name] = 'Unknow Item'
-      end
-
-      it 'エラーが返されていること' do
-        expect(subject).to eq(I18n.t('error_message.item.nonexistent_name', name: params[:item_name]))
+      it '例外が発生していること' do
+        expect do
+          subject
+        end.to raise_error(ActiveRecord::Rollback)
       end
     end
-
-    context('整数ではない数を追加しようとした場合') do
-      before do
-        params[:count] = -1
-      end
-
-      it 'エラーが返されていること' do
-        # stock_countのバリデーションンを実行するため最小限のチェックでいい
-        expect(subject.present?).to eq(true)
-      end
-    end
-  end
-
-  describe('プレイヤーのバリデーション') do
-    context('生存者ではないものが実行しようとした場合') do
+    
+    describe 'エラーメッセージの確認' do 
       subject do
         ActiveRecord::Base.transaction do
-          zombie_inventory.add!(item_name, 1)
+          inventory.add!("Unknown item", stock_count)
         end
-
-        error = zombie_inventory.errors.first
+  
+        error = inventory.errors.first
         error.messages.first
       end
 
-      let(:zombie_inventory) do
-        player = create(:player, :newcomer)
-        inventory = described_class.register_for_newcomer!(player.id, [])
-        player.update(status: Player.statuses[:zombie])
-
-        inventory
-      end
-
       it 'エラーが返されていること' do
-        i18n_key = 'error_message.inventory.inventory_control.executed_by_nonsurvivors'
-        expect(subject).to eq(I18n.t(i18n_key))
+        is_expected.to eq(I18n.t('error_message.item.nonexistent_name', name: "Unknown item"))
       end
     end
+    
   end
 end
---
+++ spec/services/inventory_control/add_spec.rb
@@ -0,0 +1,117 @@
+# frozen_string_literal: true
+
+RSpec.describe InventoryControl, type: :service do  
+  let!(:inventory) do
+    player = create(:player, :newcomer)
+    Inventory.register_for_newcomer!(player.id, [])
+  end
+  let(:item_name) { Item::Name::FIRST_AID_POUCH }
+  let(:stock_count) { 3 }
+  let(:inventory_control){described_class.new}
+
+  before do
+    SetUpper.prepare_items
+  end
+
+  describe('正常系') do
+    subject do
+      inventory_control.add(inventory, item_name, stock_count)      
+    end
+
+    context('存在しないアイテムを追加する場合') do
+      it '在庫を新規登録していること' do
+        subject
+
+        item_stock = ItemStock.all.first
+        expect(item_stock.stock_count).to eq(stock_count)
+      end
+    end
+
+    context('同じアイテムがすでに存在する場合') do
+      before do
+        item = Item.where(name: item_name).first
+        create(:item_stock, player_id: inventory.player_id, item: item, stock_count: 1)
+      end
+
+      it '在庫のカウントが増えていること' do
+        subject
+        
+        item_stock = ItemStock.all.first
+        expect(item_stock.stock_count).to eq(stock_count + 1)
+      end
+    end
+  end
+
+  xdescribe 'ロールバック' do
+    subject do
+      inventory.add!('Unknown Item', 3)
+    end
+
+    it '例外が発生していること' do
+      expect do
+        subject
+      end.to raise_error(ActiveRecord::Rollback)
+    end
+  end
+
+  describe('パラメーターの異常系') do
+    subject do
+      ActiveRecord::Base.transaction do
+        inventory.add!(params[:item_name], params[:count])
+      end
+
+      error = inventory.errors.first
+      error.messages.first
+    end
+
+    let(:params) do
+      {
+        item_name: item_name,
+        count: 1
+      }
+    end
+
+    context('存在しないアイテム名のものを追加しようとした場合') do
+      before do
+        params[:item_name] = 'Unknow Item'
+      end
+
+      it 'エラーが返されていること' do
+        is_expected.to eq(I18n.t('error_message.item.nonexistent_name', name: params[:item_name]))
+      end
+    end
+
+    context('整数ではない数を追加しようとした場合') do
+      before do
+        params[:count] = -1
+      end
+
+      it 'エラーが返されていること' do
+        # stock_countのバリデーションンを実行するため最小限のチェックでいい
+        expect(subject.present?).to eq(true)
+      end
+    end
+  end
+
+  describe('プレイヤーのバリデーション') do
+    context('生存者ではないものが実行しようとした場合') do
+      subject do        
+        error = inventory_control.add(zombie_inventory, item_name, stock_count)              
+        error.messages.first
+      end
+
+      let(:zombie_inventory) do
+        player = create(:player, :newcomer)
+        inventory = Inventory.register_for_newcomer!(player.id, [])
+        player.update(status: Player.statuses[:zombie])
+
+        inventory
+      end
+
+      it 'エラーが返されていること' do
+        i18n_key = 'error_message.inventory.inventory_control.executed_by_nonsurvivors'
+        expect(subject).to eq(I18n.t(i18n_key))
+      end
+    end
+  end
+end
